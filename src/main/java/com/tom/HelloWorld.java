5813e741470ee84e647e763eb656d051e6d1df59
Hank Tom <hanktom@gmail.com>
2021-03-09 16:19:59 +0800

    a small student project

--- src/main/java/com/tom/HelloWorld.java
+++ src/main/java/com/tom/HelloWorld.java
@@ -2,6 +2,21 @@ package com.tom;
 
 public class HelloWorld {
     public static void main(String[] args) {
+
         System.out.println("Hello world");
+//        Student stu = new Student("1101", "Eric", 60, 80);
+//        stu.print();
+//        int[] scores = {50, 80, 35, 90};
+//        int[] ss = new int[5];
+        Student[] students = {
+                new Student("1101", "Eric", 60, 80),
+                new Student("1102", "Hank", 60, 80),
+                new Student("1103", "Jane", 60, 80),
+                new Student("1106", "Jack", 60, 80)
+        };
+        for (int i = 0; i < students.length; i++) {
+            students[i].print();
+        }
+
     }
 }
--- src/main/java/com/tom/Student.java
+++ src/main/java/com/tom/Student.java
@@ -1,4 +1,17 @@
 package com.tom;
 
 public class Student {
+    String id;
+    String name;
+    int english;
+    int math;
+    public Student(String id, String name, int english, int math) {
+        this.id = id;
+        this.name = name;
+        this.english = english;
+        this.math = math;
+    }
+    public void print() {
+        System.out.println(id + "\t" + name + "\t" + english + "\t" + math + "\t" + (english+math)/2);
+    }
 }
