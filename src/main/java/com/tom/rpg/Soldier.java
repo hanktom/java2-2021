0a849678ee58577c67afa15f2848be2499ea99f2
Hank Tom <hanktom@gmail.com>
2021-03-23 16:08:35 +0800

    rpg game characters inheritant design

---
+++ src/main/java/com/tom/rpg/Soldier.java
@@ -0,0 +1,14 @@
+package com.tom.rpg;
+
+public class Soldier extends Villager {
+    public Soldier(int x, int y) {
+        super(x, y);
+        hp = 200;
+    }
+
+    @Override
+    public void walk() {
+
+
+    }
+}
---
+++ src/main/java/com/tom/rpg/Villager.java
@@ -0,0 +1,15 @@
+package com.tom.rpg;
+
+public class Villager {
+    int hp = 100;
+    int positionX;
+    int positionY;
+    public Villager(int x, int y) {
+        positionX = x;
+        positionY = y;
+    }
+    //methods
+    public void walk() {
+
+    }
+}
