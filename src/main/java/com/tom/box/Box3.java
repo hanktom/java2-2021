484bc29b2b72945bd5d04d972e3be1da6b32a33a
Hank Tom <hanktom@gmail.com>
2021-03-30 16:10:52 +0800

    better box

--- src/main/java/com/tom/box/Box3.java
+++ src/main/java/com/tom/box/Box3.java
@@ -1,4 +1,18 @@
 package com.tom.box;
 
-public class Box3 {
+public class Box3 extends Box{
+    public Box3() {
+        length = 10;
+        width = 10;
+        height = 10;
+    }
+    @Override
+    public String getName() {
+        return "BOX 3";
+    }
+
+    @Override
+    public int getPrice() {
+        return 60;
+    }
 }
--- src/main/java/com/tom/box/Tester.java
+++ src/main/java/com/tom/box/Tester.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
 
 public class Tester {
     public static void main(String[] args) {
+        Box3 box3 = new Box3();
+
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please enter object\'s length: ");
         int length = Integer.parseInt(scanner.next());
@@ -12,10 +14,14 @@ public class Tester {
         System.out.println("Please enter object\'s height: ");
         int height = Integer.parseInt(scanner.next());
         System.out.println("Length: " + length + ", width: " + width + ", height: " + height);
-        if (length <= 10 && width <= 10 && height <= 10) {
+        if (box3.validate(length, width, height)) {
+            System.out.println(box3.getName());
+        }
+
+        /*if (length <= 10 && width <= 10 && height <= 10) {
             System.out.println("Box 3 it is, price: 60");
         } else if (length <10 && width <= 20 && height <= 20) {
             System.out.println("Box 5 it is, price: 90");
-        }
+        }*/
     }
 }
