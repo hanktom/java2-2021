0e1847294574dd54b1ec368c420bfb3e3ad33614
Hank Tom <hanktom@gmail.com>
2021-04-27 16:15:32 +0800

    talk about array again

--- src/main/java/com/test/Tester.java
+++ src/main/java/com/test/Tester.java
@@ -2,13 +2,24 @@ package com.test;
 
 public class Tester {
     public static void main(String[] args) {
-        Box[] boxes = {new Box1(), new Box2(),
-                new Box3(), new Box4(), new Box5()
-        };
+        Box[] boxes = new Box[5];
+        boxes[0] = new Box1();
+        boxes[1] = new Box2();
+        boxes[2] = new Box3();
+        boxes[3] = new Box4();
+        boxes[4] = new Box5();
+        System.out.println(boxes[3].price);
+        //
+        boxes[5] = new Box1();
+
+//        Box[] boxes = {new Box1(), new Box2(),
+//                new Box3(), new Box4(), new Box5()
+//        };
         int length = 20;
         int width = 5;
         int height = 5;
         for (Box box: boxes) {
+//            Box1 box1 = (Box1) box;
             if (box.validate(length, width, height)){
                 System.out.println(box.name + " it is!, " + box.price);
                 break;
