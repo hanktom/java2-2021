584c36f3ffe0050258b4b799fb8dd8791c132058
Hank Tom <hanktom@gmail.com>
2021-05-04 16:24:12 +0800

    all about collection/boxing/heterogeneous

---
+++ src/main/java/com/tom/collection/Tester.java
@@ -0,0 +1,35 @@
+package com.tom.collection;
+
+import com.test.Box;
+import com.test.Box1;
+import com.test.Box2;
+import com.tom.stud.Student;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Tester {
+    public static void main(String[] args) {
+        int[] nums = new int[5];
+        nums[0] = 3;
+//        nums[3] = "Hank";
+//        nums[2] = true;
+        ArrayList list = new ArrayList();
+        list.add("Hank");
+        list.add(123);
+        list.add(true);
+        Box1 box = new Box1();
+        list.add(box);
+        // Heterogeneous NG!?
+        String name = (String)list.get(0);
+        String kk = (String)list.get(1);
+        //
+        Box1 box1 = new Box1();
+        Box2 box2 = new Box2();
+        ArrayList<Box> boxes = new ArrayList<Box>();
+        boxes.add(box1);
+        boxes.add(box2);
+        Box b = boxes.get(0);
+
+    }
+}
