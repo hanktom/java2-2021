1e02426b45610a3fc297ed51372b608ca42d0b7d
Hank Tom <hanktom@gmail.com>
2021-05-25 16:09:00 +0800

    about Map, key and value

--- src/main/java/com/tom/car/Tester.java
+++ src/main/java/com/tom/car/Tester.java
@@ -2,29 +2,27 @@ package com.tom.car;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 
 public class Tester {
     public static void main(String[] args) {
         int fee = 30;
-        List<Car> cars = new ArrayList<>();
-
+        Map<String, Car> cars = new HashMap<>();
         Car c1 = new Car("abc-123", "07:30");
         Car c2 = new Car("xyz-123", "09:45");
         Car c3 = new Car("ghi-123", "10:30");
-        cars.add(c1);
-        cars.add(c2);
-        cars.add(c3);
-
+        cars.put(c1.id, c1);
+        cars.put(c2.id, c2);
+        cars.put(c3.id, c3);
         String id = "abc-123";
-        for(Car c : cars) {
-            if (c.id.equals(id)) {
-
-                break;
-            }
+        Car car = cars.get(id);
+        if (car == null) {
+            System.out.println("Car not found");
+        } else {
+            Date now = new Date();
+            long ms = now.getTime() - car.enter.getTime();
+            long mins = ms/(1000*60);
+            System.out.println(mins);
         }
 
 
