dacbf764b56783097e8cd2d83d3849c5c71350ef
Hank Tom <hanktom@gmail.com>
2021-06-22 16:10:13 +0800

    Drink class design

--- src/main/java/com/tom/db/Drink.java
+++ src/main/java/com/tom/db/Drink.java
@@ -1,4 +1,18 @@
 package com.tom.db;
 
 public class Drink {
+    int id;
+    String name;
+    int price;
+
+    public Drink(int id, String name, int price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "DRINK:" + id + "/" + name + "/" + price;
+    }
 }
--- src/main/java/com/tom/db/Tester.java
+++ src/main/java/com/tom/db/Tester.java
@@ -1,6 +1,8 @@
 package com.tom.db;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Tester {
     public static void main(String[] args) {
@@ -13,17 +15,21 @@ public class Tester {
             Statement statement = conn.createStatement();
 //            statement.executeUpdate("insert into students values('000123', 'Hank')");
             ResultSet rs = statement.executeQuery("SELECT * FROM drinks");
+            List<Drink> drinks = new ArrayList<>();
             while(rs.next()) {
                 int id = rs.getInt("id");
                 String name = rs.getString("name");
                 int price = rs.getInt("price");
                 System.out.println(id + "/" + name + "/" + price);
+                drinks.add(new Drink(id, name, price));
             }
             //data collected
-
-
             //print drinks information
-
+            System.out.println("Please insert coins for drinks:");
+            for (Drink drink: drinks) {
+//                System.out.println(drink);
+                System.out.println(drink.id +") " + drink.name + "\t" + drink.price);
+            }
 
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
